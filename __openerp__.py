35ede8fb3cd07d7b068c0b5f9558b0c0b180b9cb
Marco Cid <marcocb46@gmail.com>
2016-08-27 10:30:56 -0700

    first commit of this format

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Formatos de Contabilidad Trabis',
+    'version': '1.1',
+    'category': 'Trabis',
+    'description': """
+    Este Modulo que crea formatos para las operaciones Trabis.
+    """,
+    'author': 'Samuel Martinez',
+    'website': 'http://www.trabis.com.mx',
+    'depends': ['account'],
+    'data': [
+        'report/acc_factura_cfdi.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
